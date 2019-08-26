538d0ee677822f9c0eaa36e198e821f5dd4c4b0d
Rosa <lealeon@att.net>
2019-08-26 18:45:24 +0000

    deleted extra lines

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -2,10 +2,9 @@ module Luhn
   def self.is_valid?(number)
     digits = []
     reverse = []
-    #break the credit card into individual digits.
+    
     digits = number.to_s.split("")
-    #double every second digit
-    #If the doubled value is greater than or equal to 10, subtract 9 from value.
+   
     reverse = digits.reverse
     reverse.each_with_index do | item, i | 
       if (i % 2 != 0) then 
@@ -16,10 +15,10 @@ module Luhn
       end
     end
 
-    #Sum the digits.
+    
     sum = reverse.inject(0){|sum,x| sum + x.to_i }
 
-    #If sum is divisible by 10 = valid. Otherwise, invalid.
+    
     if sum % 10 == 0
       return true
     else
